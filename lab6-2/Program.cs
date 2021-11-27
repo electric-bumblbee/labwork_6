80baa522d0677d7ac5156a03723fd479f95fa74b
Anastasia Kochkova <93382533+electric-bumblbee@users.noreply.github.com>
2021-11-27 15:55:50 +0300

    Add files via upload

---
+++ lab6-2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lab6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Лабораторная работа №6 Задание 2 (＾• ω •＾)";
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+
+            Console.WriteLine("Введите значение А");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение В");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение С");
+            int C = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine($"Прежние значения: A = {A}, B = {B}, C = {C}");
+            
+            int temp = A;
+            A = C;
+            C = B;
+            B = temp;
+
+            Console.WriteLine($"Новые значения: A = {A},  B = {B}, C = {C}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
