81610393210fed7443ab7c9b60f9ea7688459c11
Anastasia Kochkova <93382533+electric-bumblbee@users.noreply.github.com>
2021-11-27 15:58:58 +0300

    Add files via upload

---
+++ lab6-7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lab6_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Лабораторная работа №6 Задание 7 (＾• ω •＾)";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+
+            Console.WriteLine("Введите значение A");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            double n = Math.Pow(A, 5);
+            double AAA = n * n * n;
+            
+
+            Console.WriteLine($"Значение функции равно {AAA}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
